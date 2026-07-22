d2bbdd52f559cd46344b761de27167c697ff95f2
Ziad <Zsyr22@gmail.com>
2026-07-22 15:44:31 +0400

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Masroufi 💰
+      </h1>
+    </main>
+  );
+}
